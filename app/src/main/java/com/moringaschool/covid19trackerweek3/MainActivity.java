dc85809065ae42c2b66abf9438358d6927bddc73
Serahwakaba <serahwakaba9@gmail.com>
2021-07-17 10:56:45 +0300

    declare interface commit

--- app/src/main/java/com/moringaschool/covid19trackerweek3/MainActivity.java
+++ app/src/main/java/com/moringaschool/covid19trackerweek3/MainActivity.java
@@ -3,8 +3,14 @@ package com.moringaschool.covid19trackerweek3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tvCases,tvRecovered,tvCritical,tvActive,tvTodayCases,tvTotalDeaths,tvTodayDeaths,tvAffectedCountries;
+    SimpleArcLoader simpleArcLoader;
+    ScrollView scrollView;
+    PieChart pieChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
