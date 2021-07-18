f836384306eaf2e7dfde00a2ea388eed6998d261
Serahwakaba <serahwakaba9@gmail.com>
2021-07-18 16:46:54 +0300

    collecting form id commit

--- app/src/main/java/com/moringaschool/covid19trackerweek3/Login.java
+++ app/src/main/java/com/moringaschool/covid19trackerweek3/Login.java
@@ -3,12 +3,21 @@ package com.moringaschool.covid19trackerweek3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
+    private EditText mName, mSurname, mEmail,mPassword;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mName = findViewById(R.id.name);
+        mSurname = findViewById(R.id.surname);
+        mEmail = findViewById(R.id.email);
+        mPassword = findViewById(R.id.password);
+        button = findViewById(R.id.button);
     }
 }
